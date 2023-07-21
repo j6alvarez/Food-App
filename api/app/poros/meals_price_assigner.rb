34c3041d1f492f8adb01a3c2767e8820ba34f46f
Javier Alvarez <javier@paginar.net>
2023-07-21 02:26:59 -0300

    Fix category name not listed crashes

--- api/app/poros/meals_price_assigner.rb
+++ api/app/poros/meals_price_assigner.rb
@@ -5,6 +5,8 @@ require_relative 'prices_cache_maker'
 # Class to assign prices to meals
 class MealsPriceAssigner
   def self.assign_prices(meals, category)
+    return [] if meals.nil?
+
     meals.each do |meal|
       meal['price'] = PricesCacheMaker.get_or_set_price(meal, category)
     end
